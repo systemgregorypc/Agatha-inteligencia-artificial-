cd77c7ffde141b66e32d97a6c6018bb9c733ea03
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2018-02-19 05:45:20 -0400

    u

--- agathaIA-voice/agathaiaportal/ViewModel/PaggerParms.cs
+++ agathaIA-voice/agathaiaportal/ViewModel/PaggerParms.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace RinnaiPortal.Interface
+namespace AgathaiaPortal.Interface
 {
     public class PaggerParms
     {

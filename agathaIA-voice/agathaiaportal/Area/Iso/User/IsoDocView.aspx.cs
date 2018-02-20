95eb838c0d61ed8f5f7783057a641477eabc826b
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2018-02-20 01:43:24 -0400

    u

---
+++ agathaIA-voice/agathaiaportal/Area/Iso/User/IsoDocView.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RinnaiPortal.Area.Iso.User
+{
+    public partial class IsoDocView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

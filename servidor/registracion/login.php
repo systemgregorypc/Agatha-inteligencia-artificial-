c93a147c97fb9765d3c41fedfeb32c1709351aba
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2024-03-08 23:58:50 -0400

    Add files via upload

---
+++ servidor/registracion/login.php
@@ -0,0 +1,30 @@
+
+<html>
+<head>
+	<title>User registracion sysem using PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+         <h2>Iniciar sesión</h2>   
+    </div>
+
+    <form method="post" action="login.php">
+    	<div class="input-group">
+    		  <label>Username</label>
+    		  <input type="text" name="Username">
+    		</div>
+    		<div class="input-group">
+    		     <label>Password</label>
+    		     <input type="password" name="password_1">
+    		</div>
+    		<div class="input-group">
+    			<button type="submit" name="login" class="btn">login</button>
+    	    </div> 
+    	    <p> 
+    	    	 Todavía no eres miembro? <a href="registrar.php">Regístrate</a>
+    	    </p>
+    </form>
+
+</body>
+</html>
